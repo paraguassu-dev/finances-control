6a2d93e876cd4df9f8dcf8610ffeb800c7e88c99
Lucas Paraguassu <lt.paraguassu@gmail.com>
2022-05-26 21:16:23 -0300

    Plain project

---
+++ src/main/java/com/api/financescontrol/FinancesControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.financescontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/financescontrol/FinancesControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.financescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
